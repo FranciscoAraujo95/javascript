4c4cda58e2892b6b25fc225bc41bf0eebbffa4b9
Francisco Araújo <fhydglan@gmail.com>
2023-07-06 10:55:59 -0300

    Update ex009.js

--- aula011/ex009.js
+++ aula011/ex009.js
@@ -1,7 +1,9 @@
-var pais = 'Noruega';
+var pais = "Noruega";
 console.log(`Vivendo em ${pais}`);
-if (pais == 'Brasil') {
-  console.log('Você é brasileiro!');
+if (pais == "Brasil") {
+  //Condição simples
+  console.log("Você é brasileiro!");
 } else {
-  console.log('Você é estrangeiro!');
+  //Se tiver o else vira condição composta, mais de uma possibilidade
+  console.log("Você é estrangeiro!");
 }
