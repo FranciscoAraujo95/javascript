43b559d658b9879d163ed40a4e50f3e45d473277
Francisco Araújo <fhydglan@gmail.com>
2023-07-13 14:45:15 -0300

    Update script.js

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,35 +1,35 @@
-var btn = document.getElementById('btn');
-btn.addEventListener('click', contar);
-btn.style.cursor = 'pointer';
+var btn = document.getElementById("btn");
+btn.addEventListener("click", contar);
+btn.style.cursor = "pointer";
 
-var rld = document.getElementById('rld');
-rld.addEventListener('click', recarregar);
-rld.style.cursor = 'pointer';
-rld.style.color = 'gray';
+var rld = document.getElementById("rld");
+rld.addEventListener("click", recarregar);
+rld.style.cursor = "pointer";
+rld.style.color = "gray";
 
 function recarregar() {
   window.location.reload();
 }
 
 function contar() {
-  let ini = document.getElementById('txti');
-  let fim = document.getElementById('txtf');
-  let passo = document.getElementById('txtp');
-  let res = document.getElementById('res');
-  res.innerHTML = 'Contando: <br>';
+  let ini = document.getElementById("txti");
+  let fim = document.getElementById("txtf");
+  let passo = document.getElementById("txtp");
+  let res = document.getElementById("res");
+  res.innerHTML = "Contando: <br>";
   if (
     ini.value.length == 0 ||
     fim.value.length == 0 ||
     passo.value.length == 0
   ) {
-    alert('[ERRO] Faltam dados!');
-    res.innerHTML = 'Impossível contar';
+    alert("[ERRO] Faltam dados!");
+    res.innerHTML = "Impossível contar";
   } else {
     let i = Number(ini.value);
     let f = Number(fim.value);
     let p = Number(passo.value);
     if (p <= 0) {
-      alert('Passo inválido! Considerando PASSO 1');
+      alert("Passo inválido! Considerando PASSO 1");
       p = 1;
     }
     if (i < f) {
