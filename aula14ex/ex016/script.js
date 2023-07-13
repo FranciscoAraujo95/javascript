85c9b1ddd4e81170ab7c21a69d9fb9d03f9db5e9
Francisco Araújo <fhydglan@gmail.com>
2023-07-12 21:23:35 -0300

    Exercises

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,18 +1,48 @@
-var btn = document.getElementById("btn");
-btn.addEventListener("click", contar);
+var btn = document.getElementById('btn');
+btn.addEventListener('click', contar);
+btn.style.cursor = 'pointer';
+
+var rld = document.getElementById('rld');
+rld.addEventListener('click', recarregar);
+rld.style.cursor = 'pointer';
+rld.style.color = 'gray';
+
+function recarregar() {
+  window.location.reload();
+}
 
 function contar() {
-  let ini = document.getElementById("txti");
-  let fim = document.getElementById("txtf");
-  let passo = document.getElementById("txtp");
-
+  let ini = document.getElementById('txti');
+  let fim = document.getElementById('txtf');
+  let passo = document.getElementById('txtp');
+  let res = document.getElementById('res');
+  res.innerHTML = 'Contando: <br>';
   if (
     ini.value.length == 0 ||
     fim.value.length == 0 ||
     passo.value.length == 0
   ) {
-    alert("[ERRO] Faltam dados!!");
+    alert('[ERRO] Faltam dados!');
+    res.innerHTML = 'Impossível contar';
   } else {
-    alert("TUDO OK!");
+    let i = Number(ini.value);
+    let f = Number(fim.value);
+    let p = Number(passo.value);
+    if (p <= 0) {
+      alert('Passo inválido! Considerando PASSO 1');
+      p = 1;
+    }
+    if (i < f) {
+      //contagem crescente
+      for (var c = i; c <= f; c += p) {
+        res.innerHTML += `${c} \u{1F449}`;
+      }
+    } else {
+      //contagem decrescente
+      for (var c = i; c >= f; c -= p) {
+        res.innerHTML += `${c} \u{1F449}`;
+      }
+    }
+    res.innerHTML += `\u{1F3C1}`;
   }
 }
