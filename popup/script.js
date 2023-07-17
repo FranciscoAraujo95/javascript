4e5db4d16ec695bb2a478084fd6b32e51d257fab
Francisco Araújo <fhydglan@gmail.com>
2023-07-17 16:43:43 -0300

    popup exercise

--- popup/script.js
+++ popup/script.js
@@ -1,6 +1,24 @@
 function iniciaModal(modalID) {
-  const modal = document.getElementById(modalID);
-  console.log(modal);
+  if (localStorage.fechaModal !== "modal-promocao") {
+    const modal = document.getElementById(modalID);
+    if (modal) {
+      modal.classList.add("mostrar");
+      modal.addEventListener("click", (e) => {
+        if (e.target.id == "modal-promocao" || e.target.className == "fechar") {
+          modal.classList.remove("mostrar");
+          // localStorage.fechaModal = "modal-promocao";
+        }
+      });
+    }
+  }
 }
 
-iniciaModal("modal-promocao");
+const botao = document.getElementById("botao");
+botao.addEventListener("click", () => iniciaModal("modal-promocao"));
+
+document.addEventListener("scroll", () => {
+  //essa função ativa o popup quando o scroll na vertical chegar a 800
+  if (window.scrollY > 800) {
+    iniciaModal("modal-promocao");
+  }
+});
