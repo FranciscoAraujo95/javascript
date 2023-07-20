969eb3f2a4b826a950eae4beec4a9c64b33b64c2
Francisco Araújo <fhydglan@gmail.com>
2023-07-20 18:00:09 -0300

    Changes

---
+++ modelo copy/script.js
@@ -0,0 +1,27 @@
+var btn = document.getElementById("btn");
+btn.addEventListener("click", () => {
+  var num = document.getElementById("txtn");
+  var tab = document.getElementById("seltab");
+  if (num.value.length == 0) {
+    alert("Por favor digite um número!");
+  } else {
+    tab.innerHTML = "";
+    var n = Number(num.value);
+    var c = 1;
+    while (c <= 10) {
+      var item = document.createElement("option");
+      item.innerHTML = `${n} x ${c} = ${n * c}`;
+      tab.appendChild(item);
+      c++;
+    }
+  }
+});
+
+var btn1 = document.getElementById("btn1");
+btn1.addEventListener("click", () => {
+  tab.innerHTML = "Não tá dando certo";
+  tab.appendChild(item);
+});
+
+
+//como declarar uma variável?
