7801a033611f6a04d62805daee2dc15e6278812e
Francisco Araújo <fhydglan@gmail.com>
2023-07-24 20:43:29 -0300

    some new files

--- modelo copy/script.js
+++ modelo copy/script.js
@@ -1,32 +1,33 @@
-const rld = document.getElementById("rld");
-rld.addEventListener("click", () => {
+const rld = document.getElementById('rld');
+rld.addEventListener('click', () => {
   window.location.reload();
 });
 
-const btn = document.getElementById("btn");
-var vetor = [];
-btn.addEventListener("click", () => {
-  var num = document.getElementById("txtn");
-  var sel = document.getElementById("sel");
+let num = document.getElementById('txtn');
+let sel = document.getElementById('sel');
+let vetor = [];
+
+const btn = document.getElementById('btn');
+btn.addEventListener('click', () => {
   if (num.value.length == 0) {
-    alert("Número inválido!");
+    alert('Número inválido!');
   } else {
     var n = Number(num.value);
     if (n < 1 || n > 100) {
-      alert("Número inválido!");
+      alert('Número inválido!');
     } else {
-      var item = document.createElement("option");
+      var item = document.createElement('option');
       item.innerHTML = `Número ${n} adicionado.`;
       sel.appendChild(item);
       vetor.push(n);
 
-      var final = document.getElementById("final");
-      final.addEventListener("click", () => {
+      var final = document.getElementById('final');
+      final.addEventListener('click', () => {
         if (num.value.length == 0) {
-          alert("Adicione um número primeiro.");
+          alert('Adicione um número primeiro.');
         } else {
-          var p = document.createElement("p");
-          var res = document.getElementById("res");
+          var p = document.createElement('p');
+          var res = document.getElementById('res');
           p.innerHTML = `O total de números adicionados foi de ${vetor.length} <br> <br>`;
           p.innerHTML += `Os números adicionados foram ${vetor} <br> <br>`;
           res.appendChild(p);
