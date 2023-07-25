626c099d07d441332f4ed5ed8f7517f86c452f5c
Francisco Araújo <fhydglan@gmail.com>
2023-07-24 21:15:15 -0300

    solving the exercise

---
+++ aula016/ex018/script.js
@@ -0,0 +1,31 @@
+let adc = document.getElementById('adicionar');
+adc.addEventListener('click', adicionar);
+
+let num = document.querySelector('#fnum');
+let lista = document.querySelector('#flista');
+let res = document.querySelector('#res');
+let valores = [];
+
+function isNumero(n) {
+  if (Number(n) >= 1 && Number(n) <= 100) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function inLista(n, l) {
+  if (l.indexOf(Number(n)) != -1) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function adicionar() {
+  if (isNumero(num.value) && !inLista(num.value, valores)) {
+    
+  } else {
+    window.alert('Valor inválido ou já encontrado na lista.');
+  }
+}
